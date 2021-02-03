44bca1c02006b0c220eb12b3e23c65868adedb31
Felpi <felipebmagagnin@gmail.com>
2021-02-02 23:42:14 -0300

    undo

--- components/nonogram-master/src/index.js
+++
@@ -1,9 +0,0 @@
-import {Puzzle} from '../src/puzzle.js';
-import {Creator} from '../src/creator.js';
-import {Solver} from '../src/solver.js';
-import {Gui} from '../src/gui.js';
-
-
-export {Puzzle, Creator, Solver, Gui};
-
-
